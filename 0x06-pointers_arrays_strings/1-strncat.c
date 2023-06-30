cef833406c0df689c4c074aa44f6c6935abd3e8b
Thuthukile <thuthukilepear8@gmail.com>
2023-06-30 02:09:20 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings
- * using at most n bytes from src
- * @dest: input value
- * @src: input value
- * @n: input value
- * Return: dest
- */
+* _strncat - concatenates two strings
+* using at most n bytes from src
+* @dest: input value
+* @src: input value
+* @n: input value
+* Return: dest
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {
